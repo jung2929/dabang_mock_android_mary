7764eedee2c90792d0a6880efb84db8709576ba6
minhee 318 <alsgml0867@naver.com>
2020-07-18 04:15:14 +0900

    bottomNavigationBar

---
+++ dabang/app/src/main/java/com/softsquared/template/src/fragment/fragment_more.java
@@ -0,0 +1,26 @@
+package com.softsquared.template.src.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.softsquared.template.R;
+
+public class fragment_more extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_more, container, false);
+
+        return view;
+    }
+}
+
--- dabang/app/src/main/java/com/softsquared/template/src/main/MainActivity.java
+++ dabang/app/src/main/java/com/softsquared/template/src/main/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,16 +18,21 @@ import com.softsquared.template.R;
 import com.softsquared.template.src.BaseActivity;
 import com.softsquared.template.src.common.view.SimpleMessageDialog;
 import com.softsquared.template.src.common.view.TwoChoiceDialog;
+import com.softsquared.template.src.fragment.fragment_heart;
+import com.softsquared.template.src.fragment.fragment_home;
+import com.softsquared.template.src.fragment.fragment_map;
+import com.softsquared.template.src.fragment.fragment_more;
 import com.softsquared.template.src.main.interfaces.MainActivityView;
 
 public class MainActivity extends BaseActivity implements MainActivityView {
 
-    private BottomNavigationView bottomNavi;
+    private BottomNavigationView bottomNavigationView;
     private FragmentManager fm;
     private FragmentTransaction ft;
-
-
-
+    private fragment_home home;
+    private fragment_heart heart;
+    private fragment_map map;
+    private fragment_more more;
 
 
 
@@ -33,6 +40,56 @@ public class MainActivity extends BaseActivity implements MainActivityView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.action_home:
+                        setFrag(0);
+                        break;
+                    case R.id.action_heart:
+                        setFrag(1);
+                        break;
+                    case R.id.action_map:
+                        setFrag(2);
+                        break;
+                    case R.id.action_more:
+                        setFrag(3);
+                        break;
+                }
+                return true;
+            }
+        });
+        home = new fragment_home();
+        heart = new fragment_heart();
+        map = new fragment_map();
+        more = new fragment_more();
+        setFrag(0); //첫화면 지정
+    }
+
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame,home);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame,heart);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame,map);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame,more);
+                ft.commit();
+                break;
+        }
     }
 
     private void tryGetTest() {
@@ -91,5 +148,5 @@ public class MainActivity extends BaseActivity implements MainActivityView {
 //            default:
 //                break;
 //        }
-    }
+//    }
 }
